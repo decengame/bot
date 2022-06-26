9e658704a0dcf2234136028471a05273349ed868
Jeff Prestes <jeffprestes@gmail.com>
2022-06-26 09:55:57 -0400

    check player cache

--- cache/players.go
+++ cache/players.go
@@ -1,6 +1,10 @@
 package cache
 
-import "github.com/decendgame/bot/model"
+import (
+	"fmt"
+
+	"github.com/decendgame/bot/model"
+)
 
 func AddPlayer(player model.Player) {
 	ActivePlayers[player.Discord.ID] = &player
@@ -8,5 +12,10 @@ func AddPlayer(player model.Player) {
 
 func GetPlayer(playerID string) (player *model.Player, exists bool) {
 	player, exists = ActivePlayers[playerID]
+	if exists {
+		fmt.Println("Player ", player.Discord.Username, " is back")
+	} else {
+		fmt.Println("Player ", playerID, " is new")
+	}
 	return
 }
